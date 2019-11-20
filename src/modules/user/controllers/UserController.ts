d1338eaa31716d152a62985988e246a148d9a96f
darwin1224 <darwinblacks@gmail.com>
2019-11-20 23:09:25 +0700

    Fix error when post data into user and use interceptor to transform json response in get all user

--- src/modules/user/controllers/UserController.ts
+++ src/modules/user/controllers/UserController.ts
@@ -2,11 +2,21 @@ import { BadRequestException } from '@/exceptions/BadRequestException';
 import { Bcrypt } from '@/libraries/Bcrypt';
 import { Authenticate } from '@/middlewares/Authenticate';
 import { UserModel } from '@/modules/user/models/UserModel';
+import { UserResource } from '@/modules/user/resources/UserResource';
 import { UserService } from '@/modules/user/services/UserService';
-import { Body, Get, HttpCode, JsonController, Post, UseBefore } from 'routing-controllers';
+import {
+  Authorized,
+  Body,
+  Get,
+  JsonController,
+  Post,
+  UseBefore,
+  UseInterceptor,
+} from 'routing-controllers';
 
 @JsonController('/user')
 @UseBefore(Authenticate)
+@Authorized('admin')
 export class UserController {
   /**
    * Constructor
@@ -14,7 +24,7 @@ export class UserController {
    * @param {UserService} user
    * @returns {void}
    */
-  public constructor(private readonly user: UserService) {}
+  public constructor(private readonly user: UserService, private readonly bcrypt: Bcrypt) {}
 
   /**
    * Get all resource in storage
@@ -22,6 +32,7 @@ export class UserController {
    * @returns {Promise<UserModel[]>}
    */
   @Get()
+  @UseInterceptor(UserResource)
   public async index(): Promise<UserModel[]> {
     try {
       return await this.user.getAllUser();
@@ -38,15 +49,12 @@ export class UserController {
    */
   @Post()
   public async store(@Body() user: UserModel): Promise<any> {
-    const hash = Bcrypt.hash('123456');
-    console.log(hash);
-    // try {
-    //   const hash = Bcrypt.hash('123456');
-    //   console.log(hash);
-    //   const params = { ...user, password: hash };
-    //   return await this.user.insertUser(params);
-    // } catch (err) {
-    //   throw new BadRequestException(err.message);
-    // }
+    try {
+      const hash = this.bcrypt.hash(user.password);
+      const params = { ...user, password: hash };
+      return await this.user.insertUser(params);
+    } catch (err) {
+      throw new BadRequestException(err.message);
+    }
   }
 }
