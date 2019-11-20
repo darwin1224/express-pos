c8dccbc628d186c2a32be47c7e5f8c2ccefe29b1
darwin1224 <darwinblacks@gmail.com>
2019-11-20 23:08:09 +0700

    Add resource collection for supplier and user

---
+++ src/modules/supplier/resources/SupplierResource.ts
@@ -0,0 +1,4 @@
+import { BaseResourceCollection } from '@/resources/BaseResourceCollection';
+import { SupplierModel } from '@/modules/supplier/models/SupplierModel';
+
+export class SupplierResource extends BaseResourceCollection<SupplierModel> {}
---
+++ src/modules/user/resources/UserResource.ts
@@ -0,0 +1,4 @@
+import { BaseResourceCollection } from '@/resources/BaseResourceCollection';
+import { UserModel } from '@/modules/user/models/UserModel';
+
+export class UserResource extends BaseResourceCollection<UserModel> {}
