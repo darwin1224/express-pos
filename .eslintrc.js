7ca7c1117f36b43ca1c0d763e24b46e4645eeaf9
darwin1224 <darwinblacks@gmail.com>
2019-11-20 23:02:36 +0700

    Add no-unused-vars and camelcase in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,8 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/camelcase': 'off'
   },
 };
