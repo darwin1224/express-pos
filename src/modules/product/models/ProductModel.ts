ff038aa798d1d0ab640ddc38e2b7e5a4a3ef9b75
Darwin <darwinblacks@gmail.com>
2019-11-21 09:32:41 +0700

    Add product price and product stock column in product model

--- src/modules/product/models/ProductModel.ts
+++ src/modules/product/models/ProductModel.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('tbl_product')
@@ -21,4 +21,24 @@ export class ProductModel {
   @IsNotEmpty()
   @Length(4, 50)
   public readonly product_name!: string;
+
+  /**
+   * Product price column
+   *
+   * @type {number}
+   */
+  @Column({ type: 'decimal', default: 0 })
+  @IsNotEmpty()
+  @IsNumber()
+  public readonly product_price!: number;
+
+  /**
+   * Product stock column
+   *
+   * @type {number}
+   */
+  @Column({ type: 'int', unsigned: true, default: 0 })
+  @IsNotEmpty()
+  @IsNumber()
+  public readonly product_stock!: number;
 }
