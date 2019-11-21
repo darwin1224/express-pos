c7d4a6c4e2a874b9ba42f0bc3c4e583e01cb5adc
Darwin <darwinblacks@gmail.com>
2019-11-21 09:12:57 +0700

    Add read, update, delete by id in supplier

--- src/modules/supplier/controllers/SupplierController.ts
+++ src/modules/supplier/controllers/SupplierController.ts
@@ -11,7 +11,11 @@ import {
   UseBefore,
   UseInterceptor,
   Authorized,
+  Param,
+  Put,
+  Delete,
 } from 'routing-controllers';
+import { NotFoundException } from '@/exceptions/NotFoundException';
 
 @JsonController('/supplier')
 @UseBefore(Authenticate)
@@ -41,15 +45,74 @@ export class SupplierController {
   }
 
   /**
+   * Get a single resource from storage
+   *
+   * @param {number} id
+   * @returns {Promise<SupplierModel>}
+   */
+  @Get('/:id')
+  public async show(@Param('id') id: number): Promise<SupplierModel> {
+    try {
+      return await this.supplier.getSupplierById(id);
+    } catch (err) {
+      throw new NotFoundException(err.message);
+    }
+  }
+
+  /**
    * Insert a single resource into storage
    *
-   * @param {SupplierModel} params
+   * @param {SupplierModel} supplier
    * @returns {Promise<SupplierModel>}
    */
   @Post()
-  public async store(@Body() params: SupplierModel): Promise<SupplierModel> {
+  public async store(@Body() supplier: SupplierModel): Promise<SupplierModel> {
     try {
-      return await this.supplier.insertSupplier(params);
+      return await this.supplier.insertSupplier(supplier);
+    } catch (err) {
+      throw new BadRequestException(err.message);
+    }
+  }
+
+  /**
+   * Update a single resource from storage
+   *
+   * @param {number} id
+   * @param {supplier} SupplierModel
+   * @returns {Promise<SupplierModel>}
+   */
+  @Put('/:id')
+  public async update(
+    @Param('id') id: number,
+    @Body() supplier: SupplierModel,
+  ): Promise<SupplierModel> {
+    try {
+      const data = await this.supplier.getSupplierById(id);
+      if (!data) {
+        throw new NotFoundException('Data not found');
+      }
+      await this.supplier.updateSupplier(id, supplier);
+      return data;
+    } catch (err) {
+      throw new BadRequestException(err.message);
+    }
+  }
+
+  /**
+   * Delete a single resource from storage
+   *
+   * @param {number} id}
+   * @returns {Promise<SupplierModel>}
+   */
+  @Delete('/:id')
+  public async destroy(@Param('id') id: number): Promise<SupplierModel> {
+    try {
+      const data = await this.supplier.getSupplierById(id);
+      if (!data) {
+        throw new NotFoundException('Data not found');
+      }
+      await this.supplier.deleteSupplier(id);
+      return data;
     } catch (err) {
       throw new BadRequestException(err.message);
     }
--- src/modules/supplier/services/SupplierService.ts
+++ src/modules/supplier/services/SupplierService.ts
@@ -1,6 +1,6 @@
 import { SupplierModel } from '@/modules/supplier/models/SupplierModel';
 import { Service } from 'typedi';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult, DeleteResult } from 'typeorm';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 
 @Service()
@@ -25,11 +25,42 @@ export class SupplierService {
   }
 
   /**
+   * Get data by id
+   *
+   * @param {number} id
+   * @returns {Promise<SupplierModel>}
+   */
+  public getSupplierById(id: number): Promise<SupplierModel> {
+    return this.supplier.findOneOrFail(id);
+  }
+
+  /**
    * Insert data
    *
    * @returns {Promise<SupplierModel>}
    */
   public insertSupplier(params: SupplierModel): Promise<SupplierModel> {
     return this.supplier.save(params);
+  }
+
+  /**
+   * Update data by id
+   *
+   * @param {number} id
+   * @param {SupplierModel} params
+   * @returns {Promise<UpdateResult>}
+   */
+  public updateSupplier(id: number, params: SupplierModel): Promise<UpdateResult> {
+    return this.supplier.update(id, params);
+  }
+
+  /**
+   * Delete data by id
+   *
+   * @param {number} id
+   * @returns {Promise<DeleteResult>}
+   */
+  public deleteSupplier(id: number): Promise<DeleteResult> {
+    return this.supplier.delete(id);
   }
 }
