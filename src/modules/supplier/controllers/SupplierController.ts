e3a5dadd04c0d4bbab7fe097799f2a8f0b0d2777
Darwin <darwinblacks@gmail.com>
2019-11-21 08:55:27 +0700

    Add role access admin and cashier for supplier

--- src/modules/supplier/controllers/SupplierController.ts
+++ src/modules/supplier/controllers/SupplierController.ts
@@ -3,10 +3,19 @@ import { Authenticate } from '@/middlewares/Authenticate';
 import { SupplierModel } from '@/modules/supplier/models/SupplierModel';
 import { SupplierResource } from '@/modules/supplier/resources/SupplierResource';
 import { SupplierService } from '@/modules/supplier/services/SupplierService';
-import { Body, Get, JsonController, Post, UseBefore, UseInterceptor } from 'routing-controllers';
+import {
+  Body,
+  Get,
+  JsonController,
+  Post,
+  UseBefore,
+  UseInterceptor,
+  Authorized,
+} from 'routing-controllers';
 
 @JsonController('/supplier')
 @UseBefore(Authenticate)
+@Authorized(['admin', 'cashier'])
 export class SupplierController {
   /**
    * Constructor
