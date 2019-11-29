7230dfab3a397c533c0618e024bc24491aba8178
Darwin <darwinblacks@gmail.com>
2019-11-29 13:02:22 +0700

    test(unit): add all crud test for product and supplier controller

--- tests/unit/controllers/ProductController.spec.ts
+++ tests/unit/controllers/ProductController.spec.ts
@@ -2,6 +2,10 @@ import { ProductController } from '@/modules/product/controllers/ProductControll
 
 class ProductServiceMock {
   public getAllProduct: jest.Mock = jest.fn();
+  public getProductById: jest.Mock = jest.fn(() => ({ product_name: 'Gas Kompor' }));
+  public insertProduct: jest.Mock = jest.fn();
+  public updateProduct: jest.Mock = jest.fn();
+  public deleteProduct: jest.Mock = jest.fn();
 }
 
 describe('Product Controller unit tests', () => {
@@ -28,6 +32,114 @@ describe('Product Controller unit tests', () => {
 
     it('getAllProduct() should have 1 returned time', () => {
       expect(productServiceMock.getAllProduct).toHaveReturnedTimes(1);
+    });
+  });
+
+  describe('show()', () => {
+    beforeEach(async () => {
+      await product.show(1);
+    });
+
+    it('getProductById() should have been called 1 time', () => {
+      expect(productServiceMock.getProductById).toHaveBeenCalledTimes(1);
+    });
+
+    it('getProductById() should have "1" argument value', () => {
+      expect(productServiceMock.getProductById).toBeCalledWith(1);
+    });
+
+    it('getProductById() should have 1 returned time', () => {
+      expect(productServiceMock.getProductById).toHaveReturnedTimes(1);
+    });
+  });
+
+  describe('store()', () => {
+    const params = { product_name: 'Kompor', product_price: 50000, product_stock: 5 };
+
+    beforeEach(async () => {
+      await product.store(params);
+    });
+
+    it('insertProduct() should have been called 1 time', () => {
+      expect(productServiceMock.insertProduct).toHaveBeenCalledTimes(1);
+    });
+
+    it('insertProduct() should have received correct argument value', () => {
+      expect(productServiceMock.insertProduct).toBeCalledWith(params);
+    });
+
+    it('insertProduct() should have 1 returned time', () => {
+      expect(productServiceMock.insertProduct).toHaveReturnedTimes(1);
+    });
+  });
+
+  describe('update()', () => {
+    const params = { product_name: 'Gas Minyak', product_price: 50000, product_stock: 5 };
+
+    beforeEach(async () => {
+      await product.update(1, params);
+    });
+
+    it('getProductById() should have been called 1 time', () => {
+      expect(productServiceMock.getProductById).toHaveBeenCalledTimes(1);
+    });
+
+    it('getProductById() should have received correct argument value', () => {
+      expect(productServiceMock.getProductById).toBeCalledWith(1);
+    });
+
+    it('getProductById() should have 1 returned time', () => {
+      expect(productServiceMock.getProductById).toHaveReturnedTimes(1);
+    });
+
+    it('getProductById() should have received correct return value', () => {
+      expect(productServiceMock.getProductById).toHaveReturnedWith({ product_name: 'Gas Kompor' });
+    });
+
+    it('updateProduct() should have been called 1 time', () => {
+      expect(productServiceMock.updateProduct).toHaveBeenCalledTimes(1);
+    });
+
+    it('updateProduct() should have received correct argument value', () => {
+      expect(productServiceMock.updateProduct).toBeCalledWith(1, params);
+    });
+
+    it('updateProduct() should have 1 returned time', () => {
+      expect(productServiceMock.updateProduct).toHaveReturnedTimes(1);
+    });
+  });
+
+  describe('destroy()', () => {
+    beforeEach(async () => {
+      await product.destroy(1);
+    });
+
+    it('getProductById() should have been called 1 time', () => {
+      expect(productServiceMock.deleteProduct).toHaveBeenCalledTimes(1);
+    });
+
+    it('getProductById() should have received correct argument value', () => {
+      expect(productServiceMock.getProductById).toBeCalledWith(1);
+    });
+
+    it('getProductById() should have 1 returned time', () => {
+      expect(productServiceMock.getProductById).toHaveReturnedTimes(1);
+    });
+
+    it('getProductById() should have received correct return value', () => {
+      expect(productServiceMock.getProductById).toHaveReturnedWith({ product_name: 'Gas Kompor' });
+    });
+
+    it('deleteProduct() should have been called 1 time', () => {
+      expect(productServiceMock.deleteProduct).toHaveBeenCalledTimes(1);
+    });
+
+    it('deleteProduct() should have received correct argument value', () => {
+      expect(productServiceMock.deleteProduct).toBeCalledWith(1);
+    });
+
+    it('deleteProduct() should have 1 returned time', () => {
+      expect(productServiceMock.deleteProduct).toHaveReturnedTimes(1);
     });
   });
 });
--- tests/unit/controllers/SupplierController.spec.ts
+++ tests/unit/controllers/SupplierController.spec.ts
@@ -2,7 +2,10 @@ import { SupplierController } from '@/modules/supplier/controllers/SupplierContr
 
 class SupplierServiceMock {
   public getAllSupplier: jest.Mock = jest.fn();
-  public getSupplierById: jest.Mock = jest.fn();
+  public getSupplierById: jest.Mock = jest.fn(() => ({ supplier_name: 'Darwin' }));
+  public insertSupplier: jest.Mock = jest.fn();
+  public updateSupplier: jest.Mock = jest.fn();
+  public deleteSupplier: jest.Mock = jest.fn();
 }
 
 describe('Supplier Controller unit tests', () => {
@@ -47,6 +50,100 @@ describe('Supplier Controller unit tests', () => {
 
     it('getSupplierById() should have 1 returned time', () => {
       expect(supplierServiceMock.getSupplierById).toHaveReturnedTimes(1);
+    });
+  });
+
+  describe('store()', () => {
+    const params = { supplier_name: 'Kompor', supplier_price: 50000, supplier_stock: 5 };
+
+    beforeEach(async () => {
+      await supplier.store(params);
+    });
+
+    it('insertSupplier() should have been called 1 time', () => {
+      expect(supplierServiceMock.insertSupplier).toHaveBeenCalledTimes(1);
+    });
+
+    it('insertSupplier() should have received correct argument value', () => {
+      expect(supplierServiceMock.insertSupplier).toBeCalledWith(params);
+    });
+
+    it('insertSupplier() should have 1 returned time', () => {
+      expect(supplierServiceMock.insertSupplier).toHaveReturnedTimes(1);
+    });
+  });
+
+  describe('update()', () => {
+    const params = { supplier_name: 'Michael' };
+
+    beforeEach(async () => {
+      await supplier.update(1, params);
+    });
+
+    it('getSupplierById() should have been called 1 time', () => {
+      expect(supplierServiceMock.getSupplierById).toHaveBeenCalledTimes(1);
+    });
+
+    it('getSupplierById() should have received correct argument value', () => {
+      expect(supplierServiceMock.getSupplierById).toBeCalledWith(1);
+    });
+
+    it('getSupplierById() should have 1 returned time', () => {
+      expect(supplierServiceMock.getSupplierById).toHaveReturnedTimes(1);
+    });
+
+    it('getSupplierById() should have received correct return value', () => {
+      expect(supplierServiceMock.getSupplierById).toHaveReturnedWith({
+        supplier_name: 'Darwin',
+      });
+    });
+
+    it('updateSupplier() should have been called 1 time', () => {
+      expect(supplierServiceMock.updateSupplier).toHaveBeenCalledTimes(1);
+    });
+
+    it('updateSupplier() should have received correct argument value', () => {
+      expect(supplierServiceMock.updateSupplier).toBeCalledWith(1, params);
+    });
+
+    it('updateSupplier() should have 1 returned time', () => {
+      expect(supplierServiceMock.updateSupplier).toHaveReturnedTimes(1);
+    });
+  });
+
+  describe('destroy()', () => {
+    beforeEach(async () => {
+      await supplier.destroy(1);
+    });
+
+    it('getSupplierById() should have been called 1 time', () => {
+      expect(supplierServiceMock.deleteSupplier).toHaveBeenCalledTimes(1);
+    });
+
+    it('getSupplierById() should have received correct argument value', () => {
+      expect(supplierServiceMock.getSupplierById).toBeCalledWith(1);
+    });
+
+    it('getSupplierById() should have 1 returned time', () => {
+      expect(supplierServiceMock.getSupplierById).toHaveReturnedTimes(1);
+    });
+
+    it('getSupplierById() should have received correct return value', () => {
+      expect(supplierServiceMock.getSupplierById).toHaveReturnedWith({
+        supplier_name: 'Darwin',
+      });
+    });
+
+    it('deleteSupplier() should have been called 1 time', () => {
+      expect(supplierServiceMock.deleteSupplier).toHaveBeenCalledTimes(1);
+    });
+
+    it('deleteSupplier() should have received correct argument value', () => {
+      expect(supplierServiceMock.deleteSupplier).toBeCalledWith(1);
+    });
+
+    it('deleteSupplier() should have 1 returned time', () => {
+      expect(supplierServiceMock.deleteSupplier).toHaveReturnedTimes(1);
     });
   });
 });
